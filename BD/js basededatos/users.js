a6a6e84fa24ea326782c656055e429d45f2b33f2
Carlos Galvez <111470132+Carlitoz123@users.noreply.github.com>
2024-11-22 16:57:40 -0600

    AS

--- BD/js basededatos/users.js
+++ BD/js basededatos/users.js
@@ -2,20 +2,21 @@ document.getElementById("btnSave").onclick=(evt)=>{
     evt.preventDefault()//eitar recargar el form
     document.getElementById("form").classList.add("was-validated")
     Swal.fire({
-        title: "Guardado",
-        showClass: {
-          popup: `
-            animate__animated
-            animate__fadeInUp
-            animate__faster
-          `
-        },
-        hideClass: {
-          popup: `
-            animate__animated
-            animate__fadeOutDown
-            animate__faster
-          `
-        }
-      });
+  title: "Quieres guardar los cambios?",
+  showDenyButton: true,
+  showCancelButton: true,
+  confirmButtonText: "Guardado",
+  denyButtonText: `No guardado`
+}).then((result) => {
+  /* Read more about isConfirmed, isDenied below */
+  if (result.isConfirmed) {
+    Swal.fire("Guardado!", "", "Logrado");
+  } else if (result.isDenied) {
+    Swal.fire("Los cambios no se guardaron", "", "informacion");
+  }
+});
+
 }
+
+
+
